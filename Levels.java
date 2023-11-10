845606ca8c2b69dfce7e478a810c2b7e41a5bca5
NursTaal <nursultantaalaibekov01@gmail.com>
2023-11-10 09:43:04 +0600

    parsing with regex

--- Levels.java
+++ Levels.java
@@ -14,6 +14,8 @@ public class Levels {
     import java.nio.file.Paths;
     import java.nio.file.Files;
     import java.util.List;
+    import java.util.regex.Matcher;
+    import java.util.regex.Pattern;
     private String loadLevel(String levelFileName) {
         StringBuilder data = new StringBuilder();
 
@@ -21,15 +23,19 @@ public class Levels {
             Path filePath = Paths.get(levelFileName);
 
             List<String> lines = Files.readAllLines(filePath, StandardCharsets.UTF_8);
+            String pattern = "[0-4]";
+            Pattern compiledPattern = Pattern.compile(pattern);
+            Matcher matcher = null;
 
             for (String line : lines) {
-                char[] symbols = line.toCharArray();
-                for (char symbol : symbols) {
-                    if ('0' <= symbol && symbol <= '4') {
-                        data.append(symbol);
+                matcher = compiledPattern.matcher(line);
+                if(matcher.find()) {
+                    data.append(matcher.group());
+                    while(matcher.find()){
+                        data.append(matcher.group());
                     }
-                }
-                 data.append('A');
+                data.append('A');
+            }
             }
             System.out.println(data.toString());
             return data.toString();
@@ -39,5 +45,60 @@ public class Levels {
         }
 
 
-      return data.toString();
+        return data.toString();
+    }
+
+
+    //Verion 2
+    import java.io.RandomAccessFile;
+    import java.nio.ByteBuffer;
+    import java.nio.channels.FileChannel;
+    private String loadLevel(String levelFileName) {
+        StringBuilder data = new StringBuilder();
+        RandomAccessFile file = null;
+        FileChannel channel = null;
+
+        try {
+            file = new RandomAccessFile(levelFileName, "r");
+            channel = file.getChannel();
+
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            while (channel.read(buffer) != -1) {
+                buffer.flip();
+
+                while (buffer.hasRemaining()) {
+                    char symbol = (char) buffer.get();
+                    if ('0' <= symbol && symbol <= '4') {
+                        data.append(symbol);
+                    }
+
+                    if (symbol == '\n') {
+                        data.append('A');
+                    }
+                }
+
+                buffer.clear();
+            }
+
+        } catch (IOException ioe) {
+            System.out.println("Error " + ioe);
+        } finally {
+            try {
+                if (channel != null) {
+                    channel.close();
+                }
+                if (file != null) {
+                    file.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Error closing resources: " + e);
+            }
+        }
+
+        return data.toString();
+    }
+
+
+
 }
