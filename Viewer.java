39574200385f67d64b9a37cfe3716daaf3508786
invsblmad <aizatmadumarova04@gmail.com>
2023-11-11 15:25:16 +0600

    added menu panel

--- Viewer.java
+++ Viewer.java
@@ -1,26 +1,46 @@
 import javax.swing.JFrame;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class Viewer {
 
     private Controller controller;
     private Canvas canvas;
+    private JFrame frame;
+    private Image backgroundImage;
 
     public Viewer() {
         controller = new Controller(this);
         Model model = controller.getModel();
         canvas = new Canvas(model);
+
+        File backgroundFile = new File("images/background.jpg");
+        backgroundImage = getImage(backgroundFile);
     }
 
     public void runApplication() {
-        JFrame frame = new JFrame("Sokoban");
+        MenuPanel menu = new MenuPanel(backgroundImage);
+        frame = new JFrame("Sokoban");
         frame.setSize(1000, 700);
         frame.setLocation(270, 70);
-        frame.add("Center", canvas);
+        frame.add("Center", menu);
         frame.setVisible(true);
         frame.addKeyListener(controller);
     }
 
     public void update() {
 
+    }
+
+    private Image getImage(File file) {
+        Image image = null;
+        try {
+            image = ImageIO.read(file);
+        } catch (IOException e) {
+          System.out.println("Error: " + e);
+        }
+        return image;
     }
 }
