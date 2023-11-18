df986801a8a7333f1d2c4076fab84e1eac8d9c5d
Diyorka <diyorp2@mail.ru>
2023-11-18 17:03:19 +0600

    Deleted unnecessary print

--- EnemyModel.java
+++ EnemyModel.java
@@ -152,9 +152,6 @@ public class EnemyModel implements GeneralModel{
 
     public Player initPlayer(String nickname) {
         player = dbService.getPlayerInfo(nickname);
-        System.out.println(player.getNickname());
-        System.out.println(player.getAvailableSkins());
-        System.out.println(player.getTotalCoins());
         return player;
     }
 
